e63fb35eb31aaad7ffed8dd0fd9362829bf546df
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2018-04-17 02:14:02 -0500

    Implement gabime's suggestions by not over-burdening the logger with thread name information when it is not needed.
    Consequences of these changes are :
    
        1) No speed degradation of the original implementation as os::thread_name() does not have to be called on each log_msg creation.
    
        2) A further side-effect of 1) is the point gabime brought up about not needing to store the thread_name in the log_msg struct. Further, with this new scheme, async_msg does not have to be updated.
    
        3) The new '%q' flag becomes superfluous then as we are now tieing the thread_name to the presence of the msg.thread_id variable. This implies that the new q_formatter class is not needed and can be deleted. A benefit of this is, no need to update the Wiki to account for this new flag.
    
        4) The meaning of the '%t' flag now changes then to ALWAYS imply "<thread_id> = <thread_name>" in the formatter.

--- include/spdlog/details/log_msg.h
+++ include/spdlog/details/log_msg.h
@@ -14,9 +14,7 @@
 namespace spdlog {
 namespace details {
 struct log_msg
-{
-    static const size_t    RECOMMENDED_BUFFER_SIZE = 20;
-	
+{	
     log_msg() = default;
     log_msg(const std::string *loggers_name, level::level_enum lvl)
         : logger_name(loggers_name)
@@ -28,10 +26,6 @@ struct log_msg
 
 #ifndef SPDLOG_NO_THREAD_ID
         thread_id = os::thread_id();
-	
-        char theThreadName[RECOMMENDED_BUFFER_SIZE];
-        os::thread_name(theThreadName, sizeof(theThreadName));
-        thread_name = std::string(theThreadName);
 #endif
     }
 
@@ -43,7 +37,6 @@ struct log_msg
     level::level_enum level;
     log_clock::time_point time;
     size_t thread_id;
-    std::string thread_name;
     fmt::MemoryWriter raw;
     fmt::MemoryWriter formatted;
     size_t msg_id{0};
